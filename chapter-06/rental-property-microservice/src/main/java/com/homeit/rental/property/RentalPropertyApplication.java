4bd7436ae28dd40bb4759d10366f502f1d21d4be
Rodrigo Santiago <oliveira@gmail.com>
2024-06-02 02:03:00 -0300

    chapter 6 code

--- chapter-06/rental-property-microservice/src/main/java/com/homeit/rental/property/RentalPropertyApplication.java
+++ chapter-06/rental-property-microservice/src/main/java/com/homeit/rental/property/RentalPropertyApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure
 		.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class RentalPropertyApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(
-			RentalPropertyApplication.class,
-			args);
+		SpringApplication app =
+			new SpringApplication(RentalPropertyApplication.class);
+
+		app.setDefaultProperties(
+			Map.of("spring.threads.virtual.enabled", "true"));
+
+		app.run(args);
 	}
 }
+
